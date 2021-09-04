cbd59c7f6f12eecaab33ba1955b6f805a427571d
dineshkumar12004 <dineshkumar12004@gmail.com>
2021-09-04 21:10:43 +0530

    Added alternate approach

--- GFG-DSA/Java/Sorting_Algorithms/MergeSortedArray.java
+++ GFG-DSA/Java/Sorting_Algorithms/MergeSortedArray.java
@@ -2,7 +2,8 @@ class MergeSortedArray {
     public static void main(String[] args) {
         int[] nums1 = {1, 2, 3, 0, 0, 0};
         int[] nums2 = {2, 5, 6};
-        merge(nums1, 3, nums2, 3);
+        //merge(nums1, 3, nums2, 3);
+        mergeArray(nums1, 3, nums2, 3);
         for (int i = 0; i < nums1.length; i++) {
             System.out.print(nums1[i] + " ");
         }
@@ -33,5 +34,16 @@ class MergeSortedArray {
          for (int length = 0; length < n + m; length++) {
            nums1[length] = result[length];
          }
+     }
+     // Alternate approach
+     public static void mergeArray(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m-1, p2=n-1, i = m+n-1;
+        while(p2>=0){
+            if(p1>=0 && nums1[p1]>nums2[p2]){
+                nums1[i--] = nums1[p1--];
+            }else{
+                nums1[i--] = nums2[p2--];
+            }
+        }
      }
 }
