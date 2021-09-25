7831537160dadeeb2a8c2e81c94326a25c0254f7
dineshkumar12004 <dineshkumar12004@gmail.com>
2021-09-25 21:04:50 +0530

    Running sum

---
+++ Arraysum.java
@@ -0,0 +1,9 @@
+class Arraysum {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] += arr[i - 1];
+        }
+        System.out.println(arr[arr.length - 1]);
+    }
+}
