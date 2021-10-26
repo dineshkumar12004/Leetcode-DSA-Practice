baca764aaf45e95de8d62423385ef51fa779f3ca
dineshkumar12004 <dineshkumar12004@gmail.com>
2021-10-26 19:57:46 +0530

    Concatenation of Arrays

---
+++ ConcatenationofArray.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class ConcatenationofArray {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 1 };
+        int[] result = getConcatenation(nums);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public static int[] getConcatenation(int[] nums) {
+        int[] ans = new int[2 * nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = ans[i + nums.length] = nums[i];
+        }
+        return ans;
+    }
+}
