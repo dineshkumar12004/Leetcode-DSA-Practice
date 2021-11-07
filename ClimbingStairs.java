83fd564546f4b2e262818518764567acf362d749
dineshkumar12004 <dineshkumar12004@gmail.com>
2021-11-07 07:40:08 +0530

    Climbing Stairs

---
+++ ClimbingStairs.java
@@ -0,0 +1,27 @@
+class ClimbingStairs {
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println(climbStairs(n)); // 3
+    }
+
+    public static int climbStairs(int n) {
+        // base cases
+        if (n <= 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 2;
+
+        int one_step_before = 2;
+        int two_steps_before = 1;
+        int all_ways = 0;
+
+        for (int i = 2; i < n; i++) {
+            all_ways = one_step_before + two_steps_before;
+            two_steps_before = one_step_before;
+            one_step_before = all_ways;
+        }
+        return all_ways;
+    }
+}
