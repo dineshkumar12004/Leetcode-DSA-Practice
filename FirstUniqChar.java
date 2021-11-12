97c68642ac8a5575375638e32a8878d5a2d89ac0
Dinesh kumar <67814205+dineshkumar12004@users.noreply.github.com>
2021-11-12 19:31:52 +0530

    First Unique Character in a String

---
+++ FirstUniqChar.java
@@ -0,0 +1,26 @@
+// First Unique Character in a String
+class FirstUniqChar{
+  public static void (String[] args){
+    int result = firstUniqCharin("leetcode");
+    System.out.println(result);
+  }
+  public static int firstUniqCharin(String s) {
+          int[] count = new int[26];
+          int n = s.length();
+          // build char count bucket : <charIndex, count>
+          for (int i = 0; i < n; i++) {            
+              int index = s.charAt(i) - 'a';
+              count[index]++;
+          }
+
+          // find the index
+          for (int i = 0; i < n; i++) {
+              int index = s.charAt(i) - 'a';
+              if (count[index] == 1) {
+                  return i;
+              }
+
+          }
+          return -1;
+      }
+}
