1dbb4cab97135572723c99e3938016a5d521d875
Dinesh kumar <67814205+dineshkumar12004@users.noreply.github.com>
2021-11-13 19:07:58 +0530

    Create DIStringMatch

---
+++ DIStringMatch.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class DIStringMatch{
+  public static void main( String []args ) {
+    int[] result = diStringMatch("IDID");
+    System.out.println(Arrays.toString(result));
+  }
+  public static int[] diStringMatch(String S) {
+        int N = S.length();
+        int lo = 0, hi = N;
+        int[] ans = new int[N + 1];
+        for (int i = 0; i < N; ++i) {
+            if (S.charAt(i) == 'I')
+                ans[i] = lo++;
+            else
+                ans[i] = hi--;
+        }
+
+        ans[N] = lo;
+        return ans;
+    }
+}
