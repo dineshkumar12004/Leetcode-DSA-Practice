7b1efd47a5bfd84788896f73e375207698d7f9ee
dineshkumar12004 <dineshkumar12004@gmail.com>
2021-11-16 16:22:47 +0530

    Number to hex

---
+++ numtohex.py
@@ -0,0 +1,16 @@
+def toHex(num):
+    if num == 0:
+        return '0'
+    map = '0123456789abcdef'
+    result = ''
+    # if negative (two's compliment)
+    if num < 0:
+        num += 2 ** 32
+    while num > 0:
+        digit = num % 16
+        num = (num-digit) // 16
+        result += str(map[digit])
+    return result[::-1]
+
+
+print(toHex(int(input())))
