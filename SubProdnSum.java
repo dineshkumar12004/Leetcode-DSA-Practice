73e709da85e31ebf42ee19aa52404df4eb1ebc5a
dineshkumar12004 <dineshkumar12004@gmail.com>
2021-11-21 07:31:27 +0530

    Subtract the Product and Sum of Digits of an Integer

---
+++ SubProdnSum.java
@@ -0,0 +1,19 @@
+class SubProdnSum {
+    public static void main(String[] args) {
+        int n = 4421;
+        System.out.println(subtractProductAndSum(n));
+    }
+
+    public static int subtractProductAndSum(int n) {
+        int temp;
+        int sum = 0;
+        int pro = 1;
+        while (n > 0) {
+            temp = n % 10;
+            pro *= temp;
+            sum += temp;
+            n /= 10;
+        }
+        return pro - sum;
+    }
+}
