fe508411830010c70ffaa2e38ed004e5db6d7b4e
dineshkumar12004 <dineshkumar12004@gmail.com>
2021-11-30 06:41:01 +0530

    create Max69Numbers.java

---
+++ Max69Numbers.java
@@ -0,0 +1,21 @@
+class Max69Numbers {
+    public static void main(String[] args) {
+        int num = 9669;
+        System.out.println(maximum69Number(num));
+    }
+
+    public static int maximum69Number(int num) {
+
+        char[] numStr = String.valueOf(num).toCharArray();
+
+        for (int i = 0; i < numStr.length; i++) {
+            if (numStr[i] == '6') {
+                numStr[i] = '9';
+                break;
+            }
+        }
+
+        return Integer.parseInt(String.valueOf(numStr));
+    }
+
+}
