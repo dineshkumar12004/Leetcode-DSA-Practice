3cf79cd0bf9f235058b69516066b97f19960509d
dineshkumar12004 <dineshkumar12004@gmail.com>
2021-12-09 05:22:28 +0530

    Create RemoveElement.java

---
+++ JAVA/RemoveElement.java
@@ -0,0 +1,19 @@
+public class RemoveElement {
+    public static void main(String[] args) {
+        int[] nums = { 3, 2, 2, 3 };
+        int val = 3;
+        System.out.println(removeElement(nums, val));
+
+    }
+
+    public static int removeElement(int[] nums, int val) {
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+}
